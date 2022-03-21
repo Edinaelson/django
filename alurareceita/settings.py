f82342c57fe45431edca522f41afa9b4842451e4
edinaelson <sedinaelson@gmail.com>
2022-03-20 22:58:09 -0300

    atualizando templates staticos

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -121,4 +121,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alurareceita/static')
+]
