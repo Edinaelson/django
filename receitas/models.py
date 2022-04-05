1c910dd580a5d03e64de1a4e7e281707bb81d9a2
edinaelson <sedinaelson@gmail.com>
2022-04-05 18:06:58 -0300

    salvando alterações

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,13 @@
+import datetime as datetime
 from django.db import models
 
+
 # Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=200)
+    date_receita = models.DateField(default=datetime.date.today(), blank=True)
--- receitas/views.py
+++ receitas/views.py
@@ -5,7 +5,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'sopa de legumes',
+        3:'Sorvete',
+        4:'Bolo de chocolate'
+    }
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request, 'index.html',dados)
 
 def receita(request):
     return render(request, 'receita.html')
